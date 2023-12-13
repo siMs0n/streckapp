83d6c3baddd3fa338c721ec50e2c63c4f28cc339
Simon Nielsen <simon.nielsen@devies.se>
2023-12-13 14:03:51 +0100

    Fetch products from mongodb in netlify function

--- client/netlify/functions/products/products.ts
+++ client/netlify/functions/products/products.ts
@@ -1,3 +1,24 @@
+import { MongoClient } from 'mongodb';
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI as string);
+
+const clientPromise = mongoClient.connect();
+
 export default async () => {
-  return Response.json([{ name: 'Frodo' }, { name: 'Sam' }]);
+  try {
+    const database = (await clientPromise).db('streckapp');
+    const products = database.collection('products');
+
+    const results = await products.find({}).toArray();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } catch (error) {
+    return {
+      statusCode: 200,
+      body: error.toString(),
+    };
+  }
 };
