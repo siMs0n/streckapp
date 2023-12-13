7d684c68b4f56090e0364b4eece2b5395e656b8d
Simon Nielsen <simon.nielsen@devies.se>
2023-12-13 14:27:28 +0100

    Switch to lamdba style function

--- client/netlify/functions/products/products.ts
+++ client/netlify/functions/products/products.ts
@@ -1,18 +1,25 @@
 import { MongoClient } from 'mongodb';
+import type { Handler } from '@netlify/functions';
 
 const mongoClient = new MongoClient(process.env.MONGODB_URI as string);
 
 const clientPromise = mongoClient.connect();
 
-export default async () => {
+export const handler: Handler = async () => {
   try {
     const database = (await clientPromise).db('streckapp');
     const products = database.collection('products');
 
     const results = await products.find({}).toArray();
 
-    return Response.json(results);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
   } catch (error) {
-    return new Response(error.toString(), { status: 500 });
+    return {
+      statusCode: 500,
+      body: error.toString(),
+    };
   }
 };
