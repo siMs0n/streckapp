00878dff1ca6527538edeb2ed7668047710c43f0
Simon Nielsen <simon.nielsen@devies.se>
2023-12-14 19:03:17 +0100

    Use serverless functions for all non-admin endpoints

--- client/src/api/api-methods.ts
+++ client/src/api/api-methods.ts
@@ -10,7 +10,7 @@ import {
 } from '../types';
 
 const httpClient = axios.create({
-  baseURL: 'https://streckapp.onrender.com',
+  baseURL: 'https://strecklista.netlify.app/.netlify/functions',
 });
 
 export const getProducts = async (instanceId?: string): Promise<Product[]> => {
